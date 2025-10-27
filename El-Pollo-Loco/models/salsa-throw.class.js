8d7584b71f16f5786ce21302eaec0752acdec67e
BilalAcil <bilal-acil@web.de>
2025-10-27 09:57:08 +0100

    Splash Bilder eingefügt

--- El-Pollo-Loco/models/salsa-throw.class.js
+++ El-Pollo-Loco/models/salsa-throw.class.js
@@ -12,6 +12,14 @@ class SalsaThrow extends MovableObject {
     'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
   ];
 
+  IMAGES_SPLASH = [
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+    'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png'
+  ];
+
   constructor(x, y, direction) {
     super().loadImage(this.IMAGES_ROTATION[0]);
     this.loadImages(this.IMAGES_ROTATION);
