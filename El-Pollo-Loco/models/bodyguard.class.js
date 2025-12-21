4bf1e9d2d64cf3f795b7cf6bebd446de7743b139
BilalAcil <bilal-acil@web.de>
2025-12-21 06:08:21 +0100

    Bodyguard aufgräumt

--- El-Pollo-Loco/models/bodyguard.class.js
+++ El-Pollo-Loco/models/bodyguard.class.js
@@ -2,19 +2,19 @@ class Bodyguard extends MovableObject {
   height = 180;
   width = 160;
   y = 150;
+
   energy = 100;
   isDead = false;
   isJumping = false;
+  hasJumped = false;
+
   jumpInterval = null;
   attackInterval = null;
+  fallInterval = null;
+  deathAnimInterval = null;
+
   lastSpeedX = 0;
-  lastDirection = false;  // false = rechts schauen / true = links schauen
-
-
-
-  // 🔊 Sounds
-  bodyguardSound = new Audio('audio/bodyguard-sound.mp3');
-  boomSound = new Audio('audio/Boom.mp3');
+  lastDirection = false; // false=rechts, true=links
 
   IMAGE = 'img/4_enemie_boss_chicken/1_walk/G2.png';
 
@@ -28,35 +28,41 @@ class Bodyguard extends MovableObject {
     'img/4_enemie_boss_chicken/3_attack/G14.png',
     'img/4_enemie_boss_chicken/3_attack/G13.png'
   ];
-
   IMAGES_WALK = [
     'img/4_enemie_boss_chicken/1_walk/G1.png',
     'img/4_enemie_boss_chicken/1_walk/G2.png',
     'img/4_enemie_boss_chicken/1_walk/G3.png',
     'img/4_enemie_boss_chicken/1_walk/G4.png'
   ];
-
   IMAGES_HURT = [
     'img/4_enemie_boss_chicken/4_hurt/G21.png',
     'img/4_enemie_boss_chicken/4_hurt/G22.png',
     'img/4_enemie_boss_chicken/4_hurt/G23.png'
   ];
-
   IMAGES_DEAD = [
     'img/4_enemie_boss_chicken/5_dead/G24.png',
     'img/4_enemie_boss_chicken/5_dead/G25.png',
     'img/4_enemie_boss_chicken/5_dead/G26.png'
   ];
 
-
   constructor() {
     super();
+    this.initPosition();
+    this.initPhysics();
+    this.preloadAssets();
+    this.initSounds();
+  }
+
+  initPosition() {
     this.x = 4700;
     this.loadImage(this.IMAGE);
-    this.applyGravity();
-    this.hasJumped = false;
+  }
 
-    // Bilder vorladen
+  initPhysics() {
+    this.applyGravity();
+  }
+
+  preloadAssets() {
     this.loadImages(this.IMAGES_JUMP_START);
     this.loadImages(this.IMAGES_JUMP_UP);
     this.loadImages(this.IMAGES_JUMP_HOVER);
@@ -64,305 +70,340 @@ class Bodyguard extends MovableObject {
     this.loadImages(this.IMAGES_WALK);
     this.loadImages(this.IMAGES_HURT);
     this.loadImages(this.IMAGES_DEAD);
+  }
 
-    // 🔈 Hurt-Sound RICHTIG definieren!
+  initSounds() {
+    this.bodyguardSound = new Audio('audio/bodyguard-sound.mp3');
+    this.boomSound = new Audio('audio/Boom.mp3');
     this.hurtSound = new Audio('audio/bodyguard-hurt.mp3');
     this.hurtSound.volume = 0.6;
     this.hurtSound.load();
   }
 
-
   jumpToEndboss() {
-    if (this.isJumping || this.hasJumped) return;  // 🛑 DOPPEL-Schutz
-    this.isJumping = true;
-    this.hasJumped = true;    // Sobald gestartet → Flag setzen
+    if (!this.canStartJump()) return;
+    this.markJumpStarted();
+    this.playJumpStartSound();
+    this.startJumpMotion();
+    this.startJumpLoop();
+  }
 
-    // 🔊 Sound: Bodyguard springt runter
+  canStartJump() {
+    return !this.isJumping && !this.hasJumped;
+  }
+
+  markJumpStarted() {
+    this.isJumping = true;
+    this.hasJumped = true;
+  }
+
+  playJumpStartSound() {
     this.bodyguardSound.currentTime = 0;
     this.bodyguardSound.play();
+  }
 
-    // Sprungkraft
+  startJumpMotion() {
     this.speedY = 32;
     this.speedX = -12;
     this.playAnimation(this.IMAGES_JUMP_START);
-
-    // ❗ Intervall speichern (wichtig!)
-    this.jumpInterval = setInterval(() => {
-
-      if (this.speedY > 0) {
-        this.playAnimation(this.IMAGES_JUMP_UP);
-      } else {
-        this.playAnimation(this.IMAGES_JUMP_HOVER);
-      }
-
-      this.x += this.speedX;
-      this.speedX *= 0.99;
-
-      // --- LANDUNG --- 
-      if (this.speedY <= 0 && !this.isAboveGround()) {
-        this.y = 260;
-        this.speedY = 0;
-        this.speedX = 0;
-
-        this.boomSound.currentTime = 0;
-        this.boomSound.play();
-
-        if (this.world) {
-          this.world.jumpFromShock();
-
-          // ⬇️ Hier Statusbar ERST jetzt erstellen!
-          if (!this.world.bodyguardStatus) {
-            this.world.bodyguardStatus = new BodyguardStatusBar(this.world);
-            this.world.addToMap(this.world.bodyguardStatus);  // ✨ WICHTIG!
-          }
-        }
-
-        clearInterval(this.jumpInterval);
-        this.jumpInterval = null;
-
-        this.landAnimation();
-        this.isJumping = false;
-      }
-
-
-    }, 40);
   }
 
-  landAnimation() {
+  startJumpLoop() {
+    this.jumpInterval = setInterval(() => this.stepJump(), 40);
+  }
+
+  stepJump() {
+    this.updateJumpAnimation();
+    this.applyJumpMovement();
+    if (this.shouldLandNow()) this.handleLanding();
+  }
+
+  updateJumpAnimation() {
+    const imgs = this.speedY > 0 ? this.IMAGES_JUMP_UP : this.IMAGES_JUMP_HOVER;
+    this.playAnimation(imgs);
+  }
+
+  applyJumpMovement() {
+    this.x += this.speedX;
+    this.speedX *= 0.99;
+  }
+
+  shouldLandNow() {
+    return this.speedY <= 0 && !this.isAboveGround();
+  }
+
+  handleLanding() {
+    this.snapToGround();
+    this.playBoom();
+    this.triggerWorldShock();
+    this.ensureStatusBar();
+    this.stopJumpLoop();
+    this.startLandSequence();
+  }
+
+  snapToGround() {
+    this.y = 260;
+    this.speedY = 0;
+    this.speedX = 0;
+  }
+
+  playBoom() {
+    this.boomSound.currentTime = 0;
+    this.boomSound.play();
+  }
+
+  triggerWorldShock() {
+    if (this.world) this.world.jumpFromShock();
+  }
+
+  ensureStatusBar() {
+    if (!this.world || this.world.bodyguardStatus) return;
+    this.world.bodyguardStatus = new BodyguardStatusBar(this.world);
+    this.world.addToMap(this.world.bodyguardStatus);
+  }
+
+  stopJumpLoop() {
+    clearInterval(this.jumpInterval);
+    this.jumpInterval = null;
+    this.isJumping = false;
+  }
+
+  startLandSequence() {
     this.playAnimation(this.IMAGES_LAND);
-
-    setTimeout(() => {
-      this.loadImage('img/4_enemie_boss_chicken/3_attack/G13.png');
-
-      // ❗ Spieler wieder freigeben
-      if (this.world && this.world.character) {
-        this.world.character.freezeForBodyguard = false;
-      }
-
-      // Nach 1 Sekunde Angriff starten
-      setTimeout(() => {
-        this.startAttackLoop();
-      }, 1000);
-
-    }, this.IMAGES_LAND.length * 100);
+    const ms = this.IMAGES_LAND.length * 100;
+    setTimeout(() => this.finishLanding(), ms);
   }
 
+  finishLanding() {
+    this.loadImage('img/4_enemie_boss_chicken/3_attack/G13.png');
+    this.unfreezePlayer();
+    this.startAttackAfterDelay(1000);
+  }
 
+  unfreezePlayer() {
+    if (this.world?.character) this.world.character.freezeForBodyguard = false;
+  }
 
+  startAttackAfterDelay(ms) {
+    setTimeout(() => this.startAttackLoop(), ms);
+  }
 
   startAttackLoop() {
-    if (!this.hasJumped) return;                  // ❗ Nur nach dem Sprung aktiv!
-
-    if (this.attackInterval) clearInterval(this.attackInterval);
-
-    // 👉 Falls Werte aus Pause/HIT existieren → diese verwenden!
-    if (this.lastSpeedX !== 0) {
-      this.speedX = this.lastSpeedX;
-    } else if (this.speedX === 0) {
-      this.speedX = -15;                          // Standard: nach links starten
-    }
-
-    // 👉 Blickrichtung nicht überschreiben!
-    if (this.lastDirection !== undefined) {
-      this.otherDirection = this.lastDirection;
-    } else if (this.otherDirection === undefined) {
-      this.otherDirection = false;                // Standard-Richtung
-    }
-
-    this.attackInterval = setInterval(() => {
-      this.playAnimation(this.IMAGES_WALK);
-      this.x += this.speedX;
-
-      // 🔁 Normale Laufgrenzen
-      if (this.x <= 3780) {
-        this.speedX = 0;
-        setTimeout(() => {
-          this.otherDirection = true;
-          this.speedX = +15;
-        }, 200);
-      }
-
-      if (this.x >= 4330) {
-        this.speedX = 0;
-        setTimeout(() => {
-          this.otherDirection = false;
-          this.speedX = -15;
-        }, 200);
-      }
-
-      // 💾 Aktuelle Werte merken (wichtig für Pause/HIT)
-      this.lastSpeedX = this.speedX;
-      this.lastDirection = this.otherDirection;
-
-    }, 60);
+    if (!this.hasJumped) return;
+    this.resetAttackInterval();
+    this.restoreAttackState();
+    this.attackInterval = setInterval(() => this.stepAttack(), 60);
   }
 
+  resetAttackInterval() {
+    if (!this.attackInterval) return;
+    clearInterval(this.attackInterval);
+    this.attackInterval = null;
+  }
 
+  restoreAttackState() {
+    this.speedX = this.lastSpeedX !== 0 ? this.lastSpeedX : (this.speedX || -15);
+    this.otherDirection = this.lastDirection ?? this.otherDirection ?? false;
+  }
+
+  stepAttack() {
+    this.playAnimation(this.IMAGES_WALK);
+    this.x += this.speedX;
+    this.handleAttackBounds();
+    this.rememberAttackState();
+  }
+
+  handleAttackBounds() {
+    if (this.x <= 3780) return this.turnAroundAfterStop(true, +15);
+    if (this.x >= 4330) return this.turnAroundAfterStop(false, -15);
+  }
+
+  turnAroundAfterStop(direction, speed) {
+    this.speedX = 0;
+    setTimeout(() => {
+      this.otherDirection = direction;
+      this.speedX = speed;
+    }, 200);
+  }
+
+  rememberAttackState() {
+    this.lastSpeedX = this.speedX;
+    this.lastDirection = this.otherDirection;
+  }
 
   get collisionBox() {
     return {
-      x: this.x + 15,          // links etwas nach innen
-      y: this.y + 30,          // von oben nach unten versetzen
-      width: this.width - 25,  // rechts/links insgesamt 50 Pixel schmaler
-      height: this.height - 40 // oben/unten etwas kürzer
+      x: this.x + 15,
+      y: this.y + 30,
+      width: this.width - 25,
+      height: this.height - 40
     };
   }
 
-
   hit() {
     if (this.isDead) return;
-
-    // STOPPEN
-    if (this.attackInterval) {
-      clearInterval(this.attackInterval);
-      this.attackInterval = null;
-    }
-
-    // 💾 Letzte Richtung vorm Hit speichern
-    this.lastDirection = this.otherDirection;
-    this.lastSpeedX = this.speedX;
-    this.speedX = 0;
-
-    // SOUND
-    this.hurtSound.currentTime = 0;
-    this.hurtSound.play().catch(e => console.warn('Soundfehler:', e));
-
-    // SCHADEN
-    this.energy -= 25;
-
-    // 🔁 STATUS LEISTE UPDATEN
-    if (this.world && this.world.bodyguardStatus) {
-      this.world.bodyguardStatus.setPercentage(this.energy);
-    }
-
-    // ❗❗❗ FALLBACK BEI HIT ❗❗❗
-    if (this.world && this.world.character) {
-      const player = this.world.character;
-
-      // 1️⃣ Spieler steht rechts → Bodyguard NICHT zurückweichen lassen
-      if (player.x > this.x) {
-        this.otherDirection = false;  // Rechts schauen
-        this.speedX = 5;              // leicht vorwärts gehen
-      }
-      // 2️⃣ Spieler steht links → auch nicht zurückweichen
-      else {
-        this.otherDirection = true;   // Links schauen
-        this.speedX = -5;             // leicht vorwärts gehen
-      }
-    }
-    // ❗ ENDE DES FALLBACKS
-
-
-    // WENN TOT → STERBEN
-    if (this.energy <= 0) {
-      // 🎥 Kamera zurück zur ursprünglichen Endboss-Position
-      if (this.world && typeof this.world.onBodyguardDeath === 'function') {
-        this.world.onBodyguardDeath();
-      }
-
-      this.die();
-      return;
-    }
-
-    // HURT ANIMATION (bleibt gleich)
-    const FRAMES_PER_LOOP = this.IMAGES_HURT.length;
-    const LOOPS = 2;
-    const TOTAL_FRAMES = FRAMES_PER_LOOP * LOOPS;
-    const FRAME_DELAY = 100;
-
-    let frameCounter = 0;
-    const hurtInterval = setInterval(() => {
-      this.playAnimation(this.IMAGES_HURT);
-      frameCounter++;
-      if (frameCounter >= TOTAL_FRAMES) {
-        clearInterval(hurtInterval);
-        this.startAttackLoop();
-      }
-    }, FRAME_DELAY);
+    this.stopAttackForHit();
+    this.playHurtSound();
+    this.applyDamage(25);
+    this.updateStatusBar();
+    if (this.energy <= 0) return this.handleDeath();
+    this.applyHitFallbackMovement();
+    this.playHurtAnimationThenResume();
   }
 
+  stopAttackForHit() {
+    this.resetAttackInterval();
+    this.saveMotionState();
+    this.speedX = 0;
+  }
 
+  saveMotionState() {
+    this.lastDirection = this.otherDirection;
+    this.lastSpeedX = this.speedX;
+  }
+
+  playHurtSound() {
+    this.hurtSound.currentTime = 0;
+    this.hurtSound.play().catch(e => console.warn('Soundfehler:', e));
+  }
+
+  applyDamage(amount) {
+    this.energy -= amount;
+  }
+
+  updateStatusBar() {
+    this.world?.bodyguardStatus?.setPercentage(this.energy);
+  }
+
+  handleDeath() {
+    this.world?.onBodyguardDeath?.();
+    this.die();
+  }
+
+  applyHitFallbackMovement() {
+    const player = this.world?.character;
+    if (!player) return;
+    const playerRight = player.x > this.x;
+    this.otherDirection = !playerRight;
+    this.speedX = playerRight ? 5 : -5;
+  }
+
+  playHurtAnimationThenResume() {
+    const total = this.IMAGES_HURT.length * 2;
+    let frames = 0;
+    const intv = setInterval(() => {
+      this.playAnimation(this.IMAGES_HURT);
+      if (++frames >= total) return this.endHurt(intv);
+    }, 100);
+  }
+
+  endHurt(intervalId) {
+    clearInterval(intervalId);
+    this.startAttackLoop();
+  }
 
   die() {
     if (this.isDead) return;
     this.isDead = true;
-
-    // 🧹 STATUSBAR ENTFERNEN (WICHTIG!)
-    if (this.world?.bodyguardStatus) {
-      this.world.bodyguardStatus = null;
-    }
-
-    // ❗ Alle Bewegungs-Intervalle stoppen
-    if (this.attackInterval) clearInterval(this.attackInterval);
-    if (this.jumpInterval) clearInterval(this.jumpInterval);
-    this.speedX = 0; // stehen bleiben
-
-    // 🔊 Sound erst NACH 200ms abspielen
-    if (!this.dieSound) {
-      this.dieSound = new Audio('audio/bodyguard-die.mp3');
-      this.dieSound.volume = 0.5;
-    }
-    setTimeout(() => {
-      this.dieSound.currentTime = 0;
-      this.dieSound.play();
-    }, 200);
-
-    // 👉 Sofort den Fall starten
+    this.clearStatusBar();
+    this.stopAllIntervals();
+    this.speedX = 0;
+    this.playDieSoundDelayed(200);
     this.startFallingWhenDead();
   }
 
+  clearStatusBar() {
+    if (this.world?.bodyguardStatus) this.world.bodyguardStatus = null;
+  }
+
+  stopAllIntervals() {
+    this.resetAttackInterval();
+    this.stopJumpIfRunning();
+    this.stopFallIfRunning();
+    this.stopDeathAnimIfRunning();
+  }
+
+  stopJumpIfRunning() {
+    if (!this.jumpInterval) return;
+    clearInterval(this.jumpInterval);
+    this.jumpInterval = null;
+  }
+
+  stopFallIfRunning() {
+    if (!this.fallInterval) return;
+    clearInterval(this.fallInterval);
+    this.fallInterval = null;
+  }
+
+  stopDeathAnimIfRunning() {
+    if (!this.deathAnimInterval) return;
+    clearInterval(this.deathAnimInterval);
+    this.deathAnimInterval = null;
+  }
+
+  playDieSoundDelayed(ms) {
+    this.ensureDieSound();
+    setTimeout(() => this.playDieSound(), ms);
+  }
+
+  ensureDieSound() {
+    if (this.dieSound) return;
+    this.dieSound = new Audio('audio/bodyguard-die.mp3');
+    this.dieSound.volume = 0.5;
+  }
+
+  playDieSound() {
+    this.dieSound.currentTime = 0;
+    this.dieSound.play();
+  }
 
   startFallingWhenDead() {
     if (this.fallInterval) return;
-
-    let fallSpeed = 0;
-
-    // IMAGES_DEAD Endlosschleife während Fall
-    this.deathAnimInterval = setInterval(() => {
-      this.playAnimation(this.IMAGES_DEAD);
-    }, 200);  // du kannst hier z.B. 300 (langsamer) oder 100 (schneller) probieren
-
-    // GRAVITY / FALL
-    this.fallInterval = setInterval(() => {
-      if (this.isPaused || (this.world && this.world.isPaused)) return;
-
-      fallSpeed += 0.5;
-      this.y += fallSpeed;
-
-      // Wenn aus dem Bild verschwunden → stoppen & entfernen
-      if (this.y > 600) {
-        clearInterval(this.fallInterval);
-        clearInterval(this.deathAnimInterval);
-        this.removeFromWorld();
-      }
-    }, 1000 / 30); // 30 FPS
+    this.startDeathAnimLoop();
+    this.startFallLoop();
   }
 
+  startDeathAnimLoop() {
+    this.deathAnimInterval = setInterval(() => {
+      this.playAnimation(this.IMAGES_DEAD);
+    }, 200);
+  }
+
+  startFallLoop() {
+    let fallSpeed = 0;
+    this.fallInterval = setInterval(() => {
+      if (this.isGamePaused()) return;
+      fallSpeed += 0.5;
+      this.y += fallSpeed;
+      if (this.y > 600) this.finishDeathFall();
+    }, 1000 / 30);
+  }
+
+  isGamePaused() {
+    return this.isPaused || this.world?.isPaused;
+  }
+
+  finishDeathFall() {
+    this.stopFallIfRunning();
+    this.stopDeathAnimIfRunning();
+    this.removeFromWorld();
+  }
 
   removeFromWorld() {
-    if (this.world) {
-      const i = this.world.level.enemies.indexOf(this);
-      if (i > -1) {
-        this.world.level.enemies.splice(i, 1);
-      }
-    }
+    const enemies = this.world?.level?.enemies;
+    if (!enemies) return;
+    const i = enemies.indexOf(this);
+    if (i > -1) enemies.splice(i, 1);
   }
 
   pause() {
-    // Alle Bewegungs-Intervalle pausieren
-    if (this.attackInterval) clearInterval(this.attackInterval);
-    if (this.jumpInterval) clearInterval(this.jumpInterval);
-    if (this.fallInterval) clearInterval(this.fallInterval);
+    this.stopAllIntervals();
   }
 
   resume() {
-    if (!this.isDead && !this.isJumping) {
-      this.speedX = this.lastSpeedX;        // Bewegung wiederherstellen
-      this.otherDirection = this.lastDirection;
-      this.startAttackLoop();
-    }
+    if (this.isDead || this.isJumping) return;
+    this.speedX = this.lastSpeedX;
+    this.otherDirection = this.lastDirection;
+    this.startAttackLoop();
   }
-
-
-
 }
